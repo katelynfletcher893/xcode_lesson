292caff333305b756ebc836c12f8744aedaaf47c
katelyn <katelynfletcer893@gmail.com>
2022-08-06 12:40:43 -0400

    Initial Commit

---
+++ xcode_lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xcode_lesson
+//
+//  Created by Katelyn Kim Fletcher on 8/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ xcode_lessonUITests/xcode_lessonUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  xcode_lessonUITestsLaunchTests.swift
+//  xcode_lessonUITests
+//
+//  Created by Katelyn Kim Fletcher on 8/6/22.
+//
+
+import XCTest
+
+class xcode_lessonUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
